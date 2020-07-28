c739dffb5a7e0ce908cedbcd6214fcfcdf93af29
sayoni <sayonighosh2011@gmail.com>
2020-07-28 20:22:38 +0530

    adding generate_training

--- generate-training-data.py
+++ generate-training-data.py
@@ -11,3 +11,62 @@ import cv2
 #           |-- paper\
 #           |-- scissors\
 # having a folder structure like this will make it easy for you to read the images while pre-processing
+
+
+
+def get_images():
+    
+    cap = cv2.VideoCapture(0)
+    cnt1=0
+    while(cnt1<=250):
+        ret , frame = cap.read()
+        if ret == True:
+            frame = cv2.rectangle(frame,(100,100),(300,300),(0,0,255),5)
+            cv2.imshow('frame',frame)
+            #for i in range(2011):
+            cv2.imwrite('img'+str(cnt1)+'.jpg',frame[100:300,100:300])
+            cnt1=cnt1+1
+            #if cnt1==2010:
+            #    cap.release()
+            #    cv2.destroyAllWindows()
+            #    break
+                
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
+        
+    cap.release()
+    cv2.destroyAllWindows()
+
+    for i in range(50):
+        os.remove('img'+str(i)+'.jpg')
+
+
+def make_directory():
+    gesture = input('enter the gesture for which data is being collected')
+    present_dir = os.getcwd()
+    print(present_dir.split('\\')[-1])
+    print(present_dir.split('\\')[-2])
+    if present_dir.split('\\')[-2] == 'training_data':
+        os.chdir('../')
+    else:
+        os.chdir('./training_data')
+    os.mkdir(gesture)
+    os.chdir(gesture)
+    get_images()
+
+def four_image_category_collection():
+    for i in range(3):
+        make_directory()
+
+
+
+
+if __name__=="__main__":
+    import os
+    import cv2
+    import time
+
+    os.mkdir('training_data')
+    
+    four_image_category_collection()
+        
