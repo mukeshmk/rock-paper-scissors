8bdde3454f9fed63370a42c490a738d4395f87ce
Siddharth <siddharth25pandey@gmail.com>
2020-07-28 17:59:24 +0530

    Added the training dataset by Siddharth & Shreya

--- generate-training-data.py
+++ generate-training-data.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import sys
 
 # write your code here to open your laptop's camera
 # and store images for rock paper scissors
@@ -11,3 +12,67 @@ import cv2
 #           |-- paper\
 #           |-- scissors\
 # having a folder structure like this will make it easy for you to read the images while pre-processing
+
+
+#Press 'a' to start/pause the image collecting process.
+#Press 'q' to quit.
+
+
+
+try:
+    label_name = sys.argv[1]    # Inserting the label as rock/paper/scissor
+    num_samples = int(sys.argv[2]) # Inserting the second argument as number of pictures required
+except:
+    print("Arguments missing.")
+    print(desc)
+    exit(-1)
+
+IMG_SAVE_PATH = 'training_data'
+IMG_CLASS_PATH = os.path.join(IMG_SAVE_PATH, label_name)
+
+try:
+    os.mkdir(IMG_SAVE_PATH)
+except FileExistsError:
+    pass
+try:
+    os.mkdir(IMG_CLASS_PATH)
+except FileExistsError:
+    print("{} directory already exists.".format(IMG_CLASS_PATH))
+    print("All images gathered will be saved along with existing items in this folder")
+
+cap = cv2.VideoCapture(0)
+
+start = False
+count = 0
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        continue
+
+    if count == num_samples:
+        break
+
+    cv2.rectangle(frame, (100, 100), (500, 500), (255, 255, 255), 2)
+
+    if start:
+        roi = frame[100:500, 100:500]
+        save_path = os.path.join(IMG_CLASS_PATH, '{}.jpg'.format(count + 1))
+        cv2.imwrite(save_path, roi)
+        count += 1
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, "Collecting {}".format(count),
+            (5, 50), font, 0.7, (0, 255, 255), 2, cv2.LINE_AA)
+    cv2.imshow("Collecting images", frame)
+
+    k = cv2.waitKey(10)
+    if k == ord('a'):
+        start = not start
+
+    if k == ord('q'):
+        break
+
+print("\n{} image(s) saved to {}".format(count, IMG_CLASS_PATH))
+cap.release()
+cv2.destroyAllWindows()
