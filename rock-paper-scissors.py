d2ef8737ac25f98c93cb36ea545842959a3be8e0
sayoni <sayonighosh2011@gmail.com>
2020-07-31 17:00:49 +0530

    modified rock-paper-scissors

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from random import choice
 from tensorflow.keras.models import load_model
-
+from preprocessandtrain import preprocess
 # custom pre-process techniques
 # import this incase you want to resume the pre-processing techniques
 # for the model creation and model predection while playing
@@ -25,7 +25,45 @@ def main():
     # if you are chaning this make sure to update the code in the model
     model = load_model("rock-paper-scissors-model.h5")
 
+    test_list = ['paper','rock','scissors']
+    computer_choice = choice(test_list)
+
+    cap = cv2.VideoCapture(0)
+    
+    
+    while(True):
+        ret,frame = cap.read()
+        if ret == True:
+            frame = cv2.rectangle(frame,(100,100),(300,300),(0,0,255),5)
+            cv2.imshow('user_input',frame)
+            img = cv2.imwrite('img.jpg',frame[100:300,100:300])
+            
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+    img = cv2.imread('img.jpg')
+    img = preprocess(img)
+    
+    pred = model.predict(np.array([img]))
+    move_made_by_you = mapper(np.argmax(pred[0]))
+    print(move_made_by_you)
+
+    moves ={'rock':'scissors',
+            'scissors':'paper',
+            'paper' : 'rock',
+            'empty': 'empty'}
+
+    for key,val in moves.items():
+        if move_made_by_you == key and computer_choice == val:
+            print('You win')
+            print('move_by_you',move_made_by_you)
+            print('move of computer',computer_choice)
     # once you have written the code to play the game uncommend the below lines 
     # and place them where required so that the model can predict
     # pred = model.predict(np.array([img]))
     # move_made_by_you = mapper(np.argmax(pred[0]))
+
+if __name__ == "__main__":
+    main()
