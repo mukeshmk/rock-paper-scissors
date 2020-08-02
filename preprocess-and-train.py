b78d7ed7aa1872f0d43ef784b6c3082218b1cb75
sowmyareddy-27 <62344601+sowmyareddy-27@users.noreply.github.com>
2020-08-02 15:34:20 +0530

    Update preprocess-and-train.py

--- preprocess-and-train.py
+++ preprocess-and-train.py
@@ -15,7 +15,6 @@ def preprocess(img):
     dimensions = (width, height)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     img = cv2.resize(img,dimensions)
-    img = cv2.GaussianBlur(img, (5, 5), 0)
     return img
 
 # NOTE: This method will only work if you have followed the same folder strucutre as I mentioned
